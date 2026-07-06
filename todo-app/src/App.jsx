202f416099203413215709349decdf63d211981b
javed-ak <contact.javedak@gmail.com>
2026-07-06 21:11:49 +0530

    routes-defined

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,22 +1,28 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { Todos } from './components/Todos'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/todos")
-      .then(async (response) => {
-        const json = await response.json();
-        setTodos(json.todos);
-    })
-  }, [])
 
   return (
     <>
-    <Todos todos = {todos}/>
+    <MainApp />
     </>
+  )
+}
+
+const MainApp = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/signin' element={<Signin />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
